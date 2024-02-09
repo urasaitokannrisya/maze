32dfb49fa0dc0a7381fff42f4ba5badb686f1d77
urasaitokannrisya <133869422+urasaitokannrisya@users.noreply.github.com>
2024-02-10 08:29:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const maze_html=document.querySelector("#table");
 const tate=5,yoko=5;
 var px=50;
+var mukiArray=[
+	[-1,0],[0,1],[1,0],[0,-1]
+]
 var maze_Array=new Array(tate).fill(new Array(yoko).fill(0));
 let randamu=0;
 
 function hantei(y,x,muki){
-if(maze_Array[x][y]===1){
+	y+=mukiArray[muki][0];
+	x+=mukiArray[muki][1];
+	
+if(maze_Array[y][x]===1){
 return false;
 }
 if(muki===0&&y!==2){
 return false
 }
-maze_Array[x][y]=1;
+maze_Array[y][x]=1;
 return true;
 }
 
@@ -22,9 +28,8 @@ function init(){
 		
 			if(i===0||j===0||i===tate||j===yoko||(i%2===0&&j%2===0)){
 				maze_Array[i][j]=1;
-				if(!(i===0||j===0||i===tate||j===yoko)){
-				randamu=Math.floor(Math.random()*4);
-				while(!hantei(i,j,randamu)){randamu=Math.floor(Math.random()*4);};
+				if(!(i===0||j===0||i===tate||j===yoko){
+					while(!hantei(i,j,Math.floor(Math.random()*4))){};
 				}
 			}
 			//alert(`${i}${j}`)
